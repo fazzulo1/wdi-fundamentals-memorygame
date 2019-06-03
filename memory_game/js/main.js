d545b3fbf11018fd1a8531a14417b296e8644731
haroldjohan <haroldmiranda1@Harolds-Air.home>
2019-06-02 22:05:56 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -10,22 +10,32 @@ console.log("User flipped " + cardTwo);
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped King");
-
-if (cardsInPlay.length === 2){
-	console.log(" 2cards");
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	}else{
-		alert("Sorry, try again")
+function checkForMatch(){
+	if (cardsInPlay.length === 2){
+	//console.log(" 2cards");
+		if(cardsInPlay[0] === cardsInPlay[1]){
+			console.log("You found a match!");
+		}else{
+			console.log("Sorry, try again");
+		}
 	}
 }
+
+function flipCard(cardId){
+/*var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped King");*/	
+console.log("User flipped" + " " + cards[cardId])
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
 
 
 
